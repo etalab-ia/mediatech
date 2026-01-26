535160fac79d9e0ec274f94d1b7a410f5ace712d
Faheem <faheem.beg@hotmail.fr>
2026-01-26 14:12:07 +0100

    refactor: adding context manager for psql connexion

--- database/__init__.py
+++ database/__init__.py
@@ -1,6 +1,8 @@
 from .database_manage import (
+    close_connection_pool,
     create_all_tables,
     export_table_to_parquet,
+    get_connection,
     get_distinct_values,
     insert_data,
     postgres_to_qdrant,
